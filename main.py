69331bff98162fef7138131f940e865b6b4128a1
Sigte <49625118+sigma-dante@users.noreply.github.com>
2020-05-08 12:51:03 -0600

    Initial commit.

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import time
 
 
 def move_mouse():
+    xpos, ypos = 1, 1
     while True:
-        xpos, ypos = 1, 1
-        time.sleep(30)  # Move mouse every 58 seconds to prevent timeouts
         pag.moveRel(xpos, ypos)
-        xpos *= -1  # Multiply xpos by -1 to remain mouse in same position
-        ypos *= -1  # Multiply ypos by -1 to remain mouse in same position
+        xpos *= -1  # Multiply pos values by -1 to remain mouse in same position
+        ypos *= -1  # Multiply pos values by -1 to remain mouse in same position
+        time.sleep(30)  # Move mouse every 30 seconds to prevent timeouts
 
 
 while __name__ == "__main__":
